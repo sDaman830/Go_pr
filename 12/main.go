b94b07f71405b2c995054ea29dd77684d16ab68b
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-08-28 01:40:23 +0530

    final

--- 12/main.go
+++ 12/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	defer fmt.Println("world") //puts it at the end of code, stores stuff into stack and works like last in first out then at the end it empties te stack
+	fmt.Println("Hello")
 }
